bda3f28911570727c76fbcfac278e4209aa88680
gdlaming <gdlaming@wustl.edu>
2019-02-19 12:16:38 -0600

    printing, then encountering error

--- Lab2/Lab2/GameBoard.cpp
+++ Lab2/Lab2/GameBoard.cpp
@@ -91,15 +91,16 @@ int printBoard(const vector<game_piece> & board, unsigned int width, unsigned in
 	if (a != width*height) { //is this the correct way to do this?
 		return wrongBoardDimensions;
 	}
-	int bf = width - 1;
-	for (unsigned int c = 0; c < height; c++) {
-		for (unsigned int r = bf; r >= 0; r--) { //start at top lefthand corner and work across and then down
-												 //int index = (c+1)*r + c;
-			if (r > height) {
-				break;
-			}
+	//int bf = width - 1;
+	for (unsigned int c = height - 1; c >= 0; --c)  {
+		for (unsigned int r = 0; r < width; r++) { //start at top lefthand corner and work across and then down
+			
+			//width * row + col 
 			int index = width*c + r;
 			cout << board[index].display;
+			if (c > height) {
+				break;
+			}
 		}
 		cout << "" << endl;
 	}
